b543e3a191a89da498d1cc55331fb90dc04a9f5c
Frank Dieckmann <frank@lumina-verte.org>
2015-11-22 15:58:54 +0100

    seelenkalender.py: Added some comments.

--- seelenkalender.py
+++ seelenkalender.py
@@ -1,7 +1,8 @@
 print("Bitte gebe die Jahreszahl ein:")
-X = input()
-print("Du suchst nach dem Datum des Oster-Sonntags im Jahr",X)
-X = int(X)
+Jahr = input()
+print("Du suchst nach dem Datum des Oster-Sonntags im Jahr",Jahr)
+X = int(Jahr)
+##Following formulas comming from the wikipedia article https://de.wikipedia.org/wiki/Gau%C3%9Fsche_Osterformel#Eine_erg.C3.A4nzte_Osterformel
 k = X // 100
 m = 15 + (3*k + 3) // 4 - (8*k +13) // 25
 s = 2 - (3*k +3) // 4
@@ -12,8 +13,9 @@ og = 21 + d - r
 sz = 7 - (X + X // 4 + s) % 7
 oe = 7 - (og - sz) % 7
 os = og + oe
+##The easter formulas maybe amount a date greater then 31 (e.g. March 32. = April 1. etc.) 
 if os > 31:
     april = os - 31
-    print("Ostersonntag ist der",april,"te April!")
+    print("Ostersonntag ist der",april,"te April!",Jahr)
 else:
-        print("Ostersonntag ist der",os,"te März!")
+        print("Ostersonntag ist der",os,"te März!",Jahr)
